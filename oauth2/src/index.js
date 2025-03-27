434c23469757122493a68e94d8bad122456364f6
Stas Ukolov <ukoloff@gmail.com>
2025-03-27 10:16:36 +0500

    Show login form

--- oauth2/src/index.js
+++ oauth2/src/index.js
@@ -1,9 +1,29 @@
 require('dotenv').config()
 const express = require('express')
-const app = express()
+const oauth2 = require('client-oauth2')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const kcloak = new oauth2({
+  clientId: process.env.clientid,
+  clientSecret: process.env.clientsecret,
+  authorizationUri: "https://kcloak.ekb.ru/realms/omz2/protocol/openid-connect/auth",
+  accessTokenUri: "https://kcloak.ekb.ru/realms/omz2/protocol/openid-connect/token",
+  redirectUri: "http://localhost/auth/callback",
+  scopes: ['openid', 'email', 'profile'],
 })
 
+const app = express()
+
+app.get('/', home)
+app.get('/auth/', login)
+
 app.listen(process.env.port || 80)
+
+function home(req, res) {
+  res.send(`
+    <li><a href=/auth/>Auth</a>!
+    `)
+}
+
+function login(req, res) {
+  res.redirect(kcloak.code.getUri())
+}
