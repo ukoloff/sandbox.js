2e559b21cbabf74c66ea9daccccc634d3b0d7798
Stas Ukolov <ukoloff@gmail.com>
2025-03-27 11:47:07 +0500

    Add own Logoff URL

--- oauth2/src/www.js
+++ oauth2/src/www.js
@@ -14,6 +14,7 @@ const app = express()
 
 app.get('/', home)
 app.get('/auth/', login)
+app.get('/auth/off', logout)
 app.get('/auth/callback', logged)
 
 app.listen(process.env.port || 80)
@@ -21,13 +22,18 @@ app.listen(process.env.port || 80)
 function home(req, res) {
   res.send(`
     <li><a href=/auth/>Login</a>!
-    <li><a href="https://kcloak.ekb.ru/realms/omz2/protocol/openid-connect/logout">Logout</a>!
+    <li><a href=/auth/off>Logout</a>!
     `)
 }
 
 function login(req, res) {
   res.redirect(kcloak.code.getUri())
 }
+
+function logout(req, res) {
+  res.redirect("https://kcloak.ekb.ru/realms/omz2/protocol/openid-connect/logout")
+}
+
 
 async function logged(req, res) {
   let user = await kcloak.code.getToken(req.originalUrl)
