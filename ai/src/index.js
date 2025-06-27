a05449ba936cd6cef198a606717dc5c179b7ce28
Stas Ukolov <ukoloff@gmail.com>
2025-06-27 16:43:06 +0500

    Get pages content

--- ai/src/index.js
+++ ai/src/index.js
@@ -1,19 +1,25 @@
 import "./util/env.js"
 import sql from "./util/sql.js"
+import sql2it from "./util/sql2it.js"
 
 console.log("Hello, world!")
 
 let db = await sql()
-let result = await db.request()
-  .query(`
+let q = db.request()
+q.query(`
     With
       ${sql.pages},
       ${sql.spaces},
       ${sql.pagez()}
-    Select
-      Count(*) as N
+    Select top 3
+      id, md,
+      HASHBYTES('SHA2_256', md) as hash
     From pagez
+    Where
+      md is not Null
     `)
-console.log(result.recordset[0]['N'])
+for await (let row of sql2it(q)) {
+  console.log(row)
+}
 
 await db.close()
