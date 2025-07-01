243ffc11dd968375f3b609841ae34484175436dd
Stas Ukolov <ukoloff@gmail.com>
2025-07-01 09:09:06 +0500

    Split Markdown

--- ai/src/index.js
+++ ai/src/index.js
@@ -1,6 +1,7 @@
 import "./util/env.js"
 import sql from "./util/sql.js"
 import sql2it from "./util/sql2it.js"
+import {MarkdownTextSplitter} from 'langchain/text_splitter'
 
 console.log("Hello, world!")
 
@@ -21,8 +22,22 @@ q.query(`
       md is not Null
     `)
 
+let splitter = new MarkdownTextSplitter({chunkSize: 1000, chunkOverlap: 200})
+
 let N = 0
 for await (let row of sql2it(q)) {
+  let doc = {
+    pageContent: `# ${row.title}\n\n${row.md}`,
+    metadata: {
+      key: row.id,
+      hash: row.hash,
+    },
+  }
+  let docs = await splitter.invoke([doc])
+  let count = 0
+  for (let doc of docs) {
+    doc.metadata.chunk = ++count
+  }
   console.log(++N, row.id.toString('hex'), row.hash.toString('hex'), row.title)
 }
 
