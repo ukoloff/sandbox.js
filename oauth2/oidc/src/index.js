91b44ac60f4026f9fec8a47fae6287810a1a1113
Stas Ukolov <ukoloff@gmail.com>
2025-12-23 11:53:08 +0500

    Switch to Express

--- oauth2/oidc/src/index.js
+++ oauth2/oidc/src/index.js
@@ -1,9 +1,13 @@
 import { Provider } from "oidc-provider"
 import { generateKeyPair, exportJWK } from 'jose'
+import express from 'express'
+
+// https://github.com/panva/node-oidc-provider/blob/main/example/express.js
+const { PORT = 3000, ISSUER = `http://localhost:${PORT}` } = process.env
 
 await makeKeys()
 
-const provider = new Provider("http://localhost:3000", {
+const provider = new Provider(ISSUER, {
   // refer to the documentation for other available configuration
   jwks: await makeKeys(),
   clients: [
@@ -24,10 +28,10 @@ const provider = new Provider("http://localhost:3000", {
   }
 })
 
-const server = provider.listen(3000, () => {
-  console.log(
-    "oidc-provider listening on port 3000, check http://localhost:3000/.well-known/openid-configuration",
-  )
+const app = express()
+app.use(provider.callback())
+let server = app.listen(PORT, $ => {
+  console.log(`application is listening on port ${PORT}, check  ${ISSUER}/.well-known/openid-configuration`)
 })
 
 async function makeKeys() {
