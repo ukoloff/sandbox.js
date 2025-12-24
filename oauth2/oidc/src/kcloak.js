8e56c472c72e0e9e9d07ee5e3607bf9a7a9fd9cf
Stas Ukolov <ukoloff@gmail.com>
2025-12-24 11:43:47 +0500

    Build redirect URL

--- oauth2/oidc/src/kcloak.js
+++ oauth2/oidc/src/kcloak.js
@@ -1,6 +1,6 @@
 import { parseEnv } from 'node:util'
 import { Router } from 'express'
-import { discovery } from 'openid-client'
+import { buildAuthorizationUrl, calculatePKCECodeChallenge, discovery, randomNonce, randomPKCECodeVerifier, randomState } from 'openid-client'
 import { readFile } from 'node:fs/promises'
 import { join } from 'node:path'
 
@@ -16,7 +16,26 @@ kcloak.get('/callback', callback)
 
 async function login(req, res) {
   await ensureConfig()
-  res.send('Login')
+  let code_verifier = randomPKCECodeVerifier()
+  let code_challenge = await calculatePKCECodeChallenge(code_verifier)
+  let parameters = {
+    redirect_uri: 'http://localhost:3000/kcloak/callback',
+    scope: 'openid email profile',
+    code_challenge,
+    code_challenge_method: 'S256',
+  }
+  if (!config.serverMetadata().supportsPKCE()) {
+    /**
+     * We cannot be sure the server supports PKCE so we're going to use state too.
+     * Use of PKCE is backwards compatible even if the AS doesn't support it which
+     * is why we're using it regardless. Like PKCE, random state must be generated
+     * for every redirect to the authorization_endpoint.
+     */
+    state = randomState()
+    parameters.state = state
+  }
+  let redirectTo = buildAuthorizationUrl(config, parameters)
+  res.send('Login: ' + redirectTo)
 }
 
 async function logout(req, res) {
