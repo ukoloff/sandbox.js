1005bfd23c62877002d2f24059bfc19bf2ed65ae
Stas Ukolov <ukoloff@gmail.com>
2025-12-25 11:21:52 +0500

    Grant everything!

--- oauth2/oidc/src/inter.js
+++ oauth2/oidc/src/inter.js
@@ -14,11 +14,49 @@ export function install(app, aProvider) {
 async function form(req, res) {
   let details = await provider.interactionDetails(req, res)
   console.log(details)
-  res.render('login')
+  switch (details.prompt.name) {
+    case 'login':
+      res.render('login')
+      break
+    case 'consent':
+      let { grantId } = details
+      if (grantId) {
+        var grant = await provider.Grant.find(grantId)
+      } else {
+        grant = new provider.Grant({
+          accountId: details.session.accountId,
+          clientId: details.params.client_id,
+        })
+      }
+      let d = details.prompt.details
+
+      if (d.missingOIDCScope) {
+        grant.addOIDCScope(d.missingOIDCScope.join(' '));
+      }
+      if (d.missingOIDCClaims) {
+        grant.addOIDCClaims(d.missingOIDCClaims);
+      }
+      if (d.missingResourceScopes) {
+        for (const [indicator, scopes] of Object.entries(d.missingResourceScopes)) {
+          grant.addResourceScope(indicator, scopes.join(' '));
+        }
+      }
+      let grantId2 = await grant.save()
+      const consent = {};
+      if (!grantId) {
+        consent.grantId = grantId2
+      }
+      await provider.interactionFinished(req, res, { consent }, { mergeWithLastSubmission: true })
+      break
+  }
 }
 
 async function post(req, res) {
   let details = await provider.interactionDetails(req, res)
-  console.log(details)
-  res.send('POST')
+  let result = {
+    login: {
+      accountId: req.body.u,
+    }
+  }
+  await provider.interactionFinished(req, res, result, { mergeWithLastSubmission: false });
 }
