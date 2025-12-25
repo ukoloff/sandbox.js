88c32329d5b847ea0baf9b9ca27ff2070aa8cee6
Stas Ukolov <ukoloff@gmail.com>
2025-12-25 16:52:09 +0500

    Add  OpenID Connect 1.0 Standard Claims

--- oauth2/oidc/src/index.js
+++ oauth2/oidc/src/index.js
@@ -22,6 +22,14 @@ const provider = new Provider(ISSUER, {
   adapter: await adapter(),
   findAccount: inter.findAccount,
   scopes: ['openid', 'offline_access', 'email', 'profile'],
+  claims: {
+    address: ['address'],
+    email: ['email', 'email_verified'],
+    phone: ['phone_number', 'phone_number_verified'],
+    profile: ['birthdate', 'family_name', 'gender', 'given_name', 'locale',
+      'middle_name', 'name', 'nickname', 'picture', 'preferred_username',
+      'profile', 'updated_at', 'website', 'zoneinfo'],
+  },
   clients: [
     {
       client_id: "foo",
