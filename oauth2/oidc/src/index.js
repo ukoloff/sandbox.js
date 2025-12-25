651479ea1b14ac5ac0372c3d5d2d0c79dc8a997d
Stas Ukolov <ukoloff@gmail.com>
2025-12-25 11:06:50 +0500

    Parse body locally

--- oauth2/oidc/src/index.js
+++ oauth2/oidc/src/index.js
@@ -53,7 +53,6 @@ const provider = new Provider(ISSUER, {
 const app = express()
 app.set('views', join(import.meta.dirname, 'views'))
 app.set('view engine', 'pug')
-app.use(express.urlencoded({ extended: true }))
 app.use(PREFIX, provider.callback())
 app.use('/kcloak', kcloak)
 self(app)
--- oauth2/oidc/src/inter.js
+++ oauth2/oidc/src/inter.js
@@ -1,3 +1,5 @@
+import express from 'express'
+
 export async function url(ctx, interaction) {
   return `/inter/${interaction.uid}`
 }
@@ -6,7 +8,7 @@ let provider
 export function install(app, aProvider) {
   provider = aProvider
   app.get('/inter/:uid', form)
-  app.post('/inter/:uid', post)
+  app.post('/inter/:uid', express.urlencoded({ extended: true }), post)
 }
 
 async function form(req, res) {
@@ -19,5 +21,4 @@ async function post(req, res) {
   let details = await provider.interactionDetails(req, res)
   console.log(details)
   res.send('POST')
-  // res.render('login')
 }
